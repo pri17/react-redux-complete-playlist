04c1c06c865d53d80c304d01e4fb58e83680c9de
Gwen Zhou <pri17mib@gmail.com>
2020-12-11 11:15:53 +0000

    class 14

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas:[
+      {name: 'AA', age: 12, id:1 },
+      {name: 'BB', age:17, id:2 },
+      {name: 'CC', age:27, id:3}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas name="zhou" age="11"/>
+        <Ninjas nn = {this.state.ninjas}/>
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 class Ninjas extends Component{
+
   render(){
-    const {name,age} = this.props;
+    const {nn} = this.props;
+    const ninjaList = nn.map(nnn => {
+      return (
+        <div className="ninja" key={nnn.id}>
+          <div>Name: {nnn.name}</div>
+          <div>Age: {nnn.age}</div>
+        </div>
+      )
+    })
+
     return (
-      <div className="ninja">
-        <div>Name: {name}</div>
-        <div>Age: {age}</div>
-        <div>Belt: Black</div>
-      </div>
+     <div className="lllist">
+       {ninjaList}
+     </div>
     )
   }
 }
