c2f19099f6ad15ec45c8683215716749cf300eb8
Gwen Zhou <pri17mib@gmail.com>
2020-12-11 11:30:34 +0000

    class 16:Conditional Output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 
 const Ninjas = (props) =>{
+    // const {nn} = props;
+    // const ninjaList = nn.map(nnn => {
+    //   if(nnn.age>16){
+    //   return (
+    //     <div className="ninja" key={nnn.id}>
+    //       <div>Name: {nnn.name}</div>
+    //       <div>Age: {nnn.age}</div>
+    //     </div>
+    //   )
+    //   }
+    // })
+
     const {nn} = props;
     const ninjaList = nn.map(nnn => {
-      return (
-        <div className="ninja" key={nnn.id}>
-          <div>Name: {nnn.name}</div>
-          <div>Age: {nnn.age}</div>
-        </div>
-      )
+      return nnn.age>16 ? ( 
+      <div className="ninja" key={nnn.id}>
+        <div>Name: {nnn.name}</div>
+        <div>Age: {nnn.age}</div>
+    </div> 
+    ):(null);
+
     })
 
     return (
