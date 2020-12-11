3fc745ca71afb0dbb895c965fc5c87bbb7239e1b
Gwen Zhou <pri17mib@gmail.com>
2020-12-11 11:56:27 +0000

    class 17:Forms Revisited

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinjas from './AddNinjas';
 
 class App extends Component {
   state = {
@@ -15,6 +16,7 @@ class App extends Component {
       <div className="App">
         <h1>My first React app</h1>
         <Ninjas nn = {this.state.ninjas}/>
+        <AddNinjas></AddNinjas>
       </div>
     );
   }
