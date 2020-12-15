86a4a9a5061724fbb0514757edbc810997f75bb0
Gwen Zhou <pri17mib@gmail.com>
2020-12-15 11:02:29 +0000

    class 29:Using Axios

--- TodoApp/routeapp/src/components/Home.js
+++ TodoApp/routeapp/src/components/Home.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { Component} from 'react'
+import axios from 'axios'
 
-const Home = () =>{
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>lorennnnnnnn</p>
-        </div>
-    )
+class Home extends Component{
+    state ={
+        posts:[]
+    }
+   
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res =>{
+                this.setState({
+                    posts:res.data.slice(0,10)
+                })
+            }
+            )
+    }
+
+    render() {
+        const {posts} =this.state;
+        const postList = posts.length?(
+            posts.map(post=>{
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ):(
+            <div className="center">No Post Yet</div>
+        );
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                <p>{postList}</p>
+            </div>
+        )
+    }
+
 }
 
 export default Home
