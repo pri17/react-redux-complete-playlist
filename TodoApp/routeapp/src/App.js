55c36a127b275c37c8f3b39ffffa750531885f35
Gwen Zhou <pri17mib@gmail.com>
2020-12-15 11:44:28 +0000

    class30,31: Route Parameters

--- TodoApp/routeapp/src/App.js
+++ TodoApp/routeapp/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import {BrowserRouter,Route} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import Post from "./components/post";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Route exact path='/' component={Home}></Route>
       <Route path='/about' component={About}></Route>
+      <Route path='/:post_id' component={Post}></Route>
     </div>
     </BrowserRouter>
   );
